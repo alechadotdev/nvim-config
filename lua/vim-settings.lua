df75c5e8f352f010e6613102705900cf8971c2ec
alecha <alecha@tutanota.com>
2024-01-05 20:05:27 +0100

    refactor vim settings

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,10 +1,31 @@
-vim.cmd("set rnu")
-vim.cmd("set nu")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+vim.wo.relativenumber = true
+vim.o.expandtab = true
+-- set tab width, to automatically detect with use "tpope/vim-sleuth"
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+-- Enable mouse mode
+vim.o.mouse = "a"
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = "menuone,noselect"
+
+vim.o.termguicolors = true
 
 -- save and close files with leader
 vim.keymap.set("n", "<leader>w", ":w<CR>")
@@ -12,3 +33,19 @@ vim.keymap.set("n", "<leader>q", ":q<CR>")
 
 -- disable search highlights
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
