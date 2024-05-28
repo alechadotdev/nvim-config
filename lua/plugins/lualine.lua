1428847114ea12ee8143e71afab3c998983eee25
alecha <alecha@tutanota.com>
2024-05-28 02:34:28 +0200

    Configure lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,9 @@ return {
 	opts = {
 		options = {
 			theme = "dracula",
+            components_separator = { left = '', right = ''},
+            section_separators = { left = '', right = '' },
+            globalstatus = true,
 		},
 	},
 }
