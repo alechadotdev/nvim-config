32fae412d441ac6aecf6540dfa987cd456f1f88f
alecha <alecha@tutanota.com>
2024-05-29 03:19:29 +0200

    refactor: change symbol used by git signs to full block

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -3,12 +3,12 @@ return {
 	config = function()
 		require("gitsigns").setup({
             signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "▎" },
-                topdelete = { text = "▎" },
-                changedelete = { text = "▎" },
-                untracked = { text = "▎" },
+                add = { text = "█" },
+                change = { text = "█" },
+                delete = { text = "█" },
+                topdelete = { text = "█" },
+                changedelete = { text = "█" },
+                untracked = { text = "█" },
             },
             current_line_blame_opts = {
                 delay = 500,
