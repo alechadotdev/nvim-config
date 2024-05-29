31543209cba2c81a429e549b8a7baf102309bdef
alecha <alecha@tutanota.com>
2024-05-29 02:16:59 +0200

    feat: udpate gitsings symbols and reduce blame delay

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -1,6 +1,18 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup({})
+		require("gitsigns").setup({
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "▎" },
+                topdelete = { text = "▎" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+            current_line_blame_opts = {
+                delay = 500,
+            }
+        })
 	end,
 }
