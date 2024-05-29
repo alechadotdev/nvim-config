09c26b7a5be3ed8f8be12e6c57bf5ed7e50c3bdc
alecha <alecha@tutanota.com>
2024-05-29 03:19:54 +0200

    change theme to gruvbox-baby

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,8 @@ return {
 	"nvim-lualine/lualine.nvim",
 	opts = {
 		options = {
-			theme = "dracula",
+			-- theme = "dracula",
+            theme = "gruvbox",
             components_separator = { left = '', right = ''},
             section_separators = { left = '', right = '' },
             globalstatus = true,
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,14 +6,26 @@
 --         vim.cmd.colorscheme("catppuccin")
 --     end,
 -- }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	priority = 1000,
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			disable_background = true,
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
+
 return {
-	"rose-pine/neovim",
-	priority = 1000,
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			disable_background = true,
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
+    "luisiacc/gruvbox-baby",
+    priority = 1000,
+    name = "gruvbox-baby",
+    config = function()
+        -- vim.g.gruvbox_baby_background_color = "medium"
+        vim.g.gruvbox_baby_transparent_mode = true
+        vim.cmd("colorscheme gruvbox-baby")
+    end,
 }
