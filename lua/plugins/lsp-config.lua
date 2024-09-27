62c81d9881cbfe416132a314b8fd5b4e82cfaff3
alecha <alecha@tutanota.com>
2024-09-27 04:23:49 +0200

    fix: remove none-ls and tsserver

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,6 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.tsserver.setup({ capabilities = capabilities })
             lspconfig.pyright.setup({ capabilities = capabilities })
             lspconfig.bashls.setup({ capabilities = capabilities })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
